acd2dd2d67bfee9d9183cb465a887dd243410876
camjo09 <61036238+camjo09@users.noreply.github.com>
2020-03-11 16:26:23 +1100

    yeetus

---
+++ Doodle - Dash/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 5f;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 vel = rb.velocity;
+        vel.x = speed;
+        rb.velocity = vel;
+
+    }
+}
--- Doodle - Dash/Assets/Scripts/PlayerControls.cs
+++ Doodle - Dash/Assets/Scripts/PlayerControls.cs
@@ -7,23 +7,42 @@ public class PlayerControls : MonoBehaviour
 
 
     [Header("Physics")]
-    public CharacterController controller;
     public float gravity = 20f;
     [Header("Movement Variables")]
     public float speed = 5f;
     public Vector2 moveDirection;
-
+    public Rigidbody2D rb;
+    public float jumpSpeed = 10f;
+    int jumpCount = 1;
 
 
     void Start()
     {
-        controller = this.gameObject.GetComponent<CharacterController>();
+
     }
 
     void Update()
     {
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+
+        Vector3 vel = rb.velocity;
+        vel.x = speed;
+        rb.velocity = vel;
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < 3)
+        {
+            rb.AddForce(transform.up * jumpSpeed, ForceMode2D.Impulse);
+            jumpCount++;
+        }
+
+
+    //Need to reset jump count when touching ground
+
+    }
+    // Cleans Code: CTRL + K, CTRL + D
+    private void FixedUpdate()
+    {
 
     }
 }
+
