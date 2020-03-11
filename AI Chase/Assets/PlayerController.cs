9abc715c0fb7968b83a6cf399d202f1ac88823f8
camjo09 <61036238+camjo09@users.noreply.github.com>
2020-03-11 13:41:13 +1100

    chase

---
+++ AI Chase/Assets/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    #region Variables
+    public float Health = 0;
+
+    public float Speed;
+    public PathfindingActual playerController;
+    private Rigidbody2D _PlayerRigidBody;
+
+    private float _MoveHorizontal;
+    private float _MoveVertical;
+
+    #endregion
+    void Start()
+    #region Rigidbody
+    {
+        _PlayerRigidBody = GetComponent<Rigidbody2D>();
+    }
+    #endregion
+   
+    public void TakeDamage(float damage)
+    #region Cheeky Damage 
+    {
+        Health -= damage;
+    }
+    #endregion
+
+    private void Update()
+    {
+        if((Vector2.Distance(playerController.playerObject.transform.position, playerController.AiSprite.transform.position) < playerController.ChasePlayerDistance) && Input.GetKeyDown(KeyCode.K))
+        {
+            
+            playerController.AiHealth -= 1;
+
+
+        }
+        else
+        {
+
+        }
+    }
+    void FixedUpdate()
+     #region Moving Around 
+    {
+        if (Health > 0)
+        {
+            _MoveHorizontal = Input.GetAxis("Horizontal");
+            _MoveVertical = Input.GetAxis("Vertical");
+            Vector2 movement = new Vector2(_MoveHorizontal, _MoveVertical);
+
+            _PlayerRigidBody.AddForce(movement * Speed);
+        }
+        else
+        {
+        
+        }
+        #endregion
+
+
+    }
+}
---
+++ AI Chase/Assets/PlayerStats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{   
+    
+private float currentHealth;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
