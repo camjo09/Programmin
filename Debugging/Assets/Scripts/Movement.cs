9e0092a627e43d5c2233b683f6827a9e79f8cdcd
camjo09 <61036238+camjo09@users.noreply.github.com>
2020-03-13 12:41:49 +1100

    Bimng

---
+++ Debugging/Assets/Scripts/Movement.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+
+
+
+    [AddComponentMenu("RPG/Player/Movement")]
+    [RequireComponent(typeof(CharacterController))]
+    public class Movement : MonoBehaviour
+    {
+        [Header("Speed Vars")]
+        //Value Variables 
+        public float moveSpeed = 5;
+        public float walkSpeed = 5;
+        public float runSpeed = 10;
+        public float crouchSpeed = 2.5f; 
+        public float jumpSpeed = 10;
+        private float _gravity = 20;
+        public CharacterController charC;
+        //Struct - Contains Multiple Variables (eg...3 floats)
+        public Vector3 _moveDir;
+        //Reference Variable
+
+        private void Start()
+        {
+
+        }
+        private void Update()
+        {
+            Move();
+        }
+        private void Move()
+        {
+            //if we are on the ground
+            if (charC.isGrounded)
+            {
+                //set speed
+                if (Input.GetButton("Sprint"))
+                {
+                    moveSpeed = runSpeed;
+                }
+                
+                else if (Input.GetButton("Crouch"))
+                {
+                    moveSpeed = crouchSpeed;
+                }
+                else
+                {
+                    moveSpeed = walkSpeed;
+                }
+                //calculate movement direction based off inputs
+             
+                _moveDir = transform.TransformDirection(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) );
+                _moveDir *= moveSpeed;
+                if (Input.GetButton("Jump"))
+                {
+                    _moveDir.y = jumpSpeed;
+                }
+            }
+            //Regardless if we are grounded or not
+            //apply gravity
+            _moveDir.y -= _gravity * Time.deltaTime;
+            //apply movement
+            charC.Move(_moveDir * Time.deltaTime);
+        }
+    }
+
+
---
+++ Intro PRG - Jonah/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Game Systems/RPG/Player/Movement")]
+[RequireComponent(typeof(CharacterController))]
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Physics")]
+    public CharacterController controller;
+    public float gravity = 20f;
+    [Header("Movement Variables")]
+    public float speed = 5f;
+    public float sprintspeed = 10f;
+    public float crouchspeed = 2.5f;
+    public float jumpSpeed = 8f;                        
+    public Vector3 moveDirection;
+    public Vector3 moveD;
+    public static Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
+    [System.Serializable]
+    public struct PlayerInputs
+    {
+        public KeyCode Forward;
+        public KeyCode Left;
+        public KeyCode Right;
+        public KeyCode Backwards;
+        public KeyCode Jump;
+        public KeyCode Sprint;
+        public KeyCode Crouch;
+    }
+    public PlayerInputs playerInput;
+
+    void Start()
+    {
+        playerInput.Forward = KeybindManager.keys["Forward"];
+        playerInput.Left = KeybindManager.keys["Left"];
+        playerInput.Right = KeybindManager.keys["Right"];
+        playerInput.Backwards = KeybindManager.keys["Backwards"];
+        playerInput.Jump = KeybindManager.keys["Jump"];
+        playerInput.Sprint = KeybindManager.keys["Sprint"];
+        playerInput.Crouch = KeybindManager.keys["Crouch"];
+
+        controller = this.gameObject.GetComponent<CharacterController>();
+    }
+  
+    
+    #region Update Function
+
+
+    void Update()
+    {              
+        if (controller.isGrounded && !Stats.BaseStats.isDead)
+        {
+            //Speed
+            if (Input.GetKeyDown(keys["Sprint"]))
+            {
+                Debug.Log("Sprint");
+            }
+            if (Input.GetKeyDown(keys["Crouch"]))
+            {
+                Debug.Log("Crouch");
+            }
+
+            //Direction
+            if (Input.GetKeyDown(keys["Forward"]))
+            {
+                Debug.Log("Up");
+                moveD.z = 1;
+            }
+            if (Input.GetKeyDown(keys["Left"]))
+            {
+                Debug.Log("Left");
+            }
+            if (Input.GetKeyDown(keys["Right"]))
+            {
+                Debug.Log("Right");
+            }
+            if (Input.GetKeyDown(keys["Backwards"]))
+            {
+                Debug.Log("Backwards");
+                moveD.z = -1;
+
+            }
+
+            moveDirection = transform.TransformDirection(moveD);
+            moveDirection *= speed;
+
+            if (Input.GetKeyDown(keys["Jump"]))
+            {
+                Debug.Log("Jump");
+                moveDirection.y = jumpSpeed;
+
+            }
+          
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = sprintspeed;
+        }
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            speed = crouchspeed;
+        }
+        else
+        {
+            speed = 5f;
+        }
+    }
+    #endregion
+}
